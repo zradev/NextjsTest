29b2315c4d1a93fcec7b7bccd41a64920fd4767c
zradev <zlatkoradevv@gmail.com>
2022-10-01 16:27:50 +0300

    Add Card Component in collections folder

--- collections/Card/Card.jsx
+++ collections/Card/Card.jsx
@@ -1,1 +1,25 @@
 // The Card to be exported goes here
+
+import Image from "next/image";
+
+import {
+  StyledContainer,
+  StyledImageContainer,
+  StyledTextContainer,
+  StyledTitle,
+  StyledDescription,
+} from "./elements";
+
+export const Card = ({ image, title, description, ...props }) => {
+  return (
+    <StyledContainer {...props}>
+      <StyledImageContainer width={image.width} height={image.height}>
+        <Image src={image.src} alt={image.alt} width="100%" height="100%" />
+      </StyledImageContainer>
+      <StyledTextContainer>
+        <StyledTitle>{title}</StyledTitle>
+        <StyledDescription>{description}</StyledDescription>
+      </StyledTextContainer>
+    </StyledContainer>
+  );
+};
--- collections/Card/elements.jsx
+++ collections/Card/elements.jsx
@@ -1,1 +1,48 @@
-// Styled elements for the Card go here
+import styled from "styled-components";
+import {
+  SectionContainer,
+  SectionHeading,
+  SectionSubheading,
+} from "~/components";
+
+export const StyledContainer = styled((props) => (
+  <SectionContainer {...props} />
+))`
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin: 15px 0 15px 0;
+`;
+
+export const StyledImageContainer = styled(({ height, width, ...props }) => (
+  <div {...props} />
+))`
+  width: ${({ width }) => width}px;
+  height: ${({ height }) => height}px;
+  margin: 40px;
+`;
+
+export const StyledTextContainer = styled((props) => <div {...props} />)`
+  color: black;
+  width: 65%;
+  font-family: sans-serif;
+  margin-right: 40px;
+`;
+
+export const StyledTitle = styled((props) => <SectionHeading {...props} />)`
+  margin: 0;
+  color: black;
+  font-size: 18px;
+  line-height: 1.8rem;
+  font-weight: bold;
+  font-weight: 700;
+`;
+
+export const StyledDescription = styled((props) => (
+  <SectionSubheading {...props} />
+))`
+  margin: 0;
+  font-size: 16px;
+  line-height: 1.5rem;
+  font-weight: 500;
+`;
