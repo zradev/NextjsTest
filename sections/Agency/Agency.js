ec7887fc4df08be88507c5b9cf492c5f110bb542
zradev <zlatkoradevv@gmail.com>
2022-10-01 17:08:59 +0300

    Add Agency Component

--- sections/Agency/Agency.js
+++ sections/Agency/Agency.js
@@ -1,7 +1,40 @@
 import Image from "next/image";
 
-import { StyledAgency } from "./elements";
+import {
+  StyledContainer,
+  StyledTextContainer,
+  StyledImageContainer,
+  StyledCardsContainer,
+  StyledTitle,
+  StyledDescription,
+  StyledCard,
+} from "./elements";
 
-export const Agency = () => {
-  return <StyledAgency />;
+export const Agency = ({ title, description, videoImage, cards, ...props }) => {
+  return (
+    <>
+      <StyledContainer {...props}>
+        <StyledTextContainer>
+          <StyledTitle>{title}</StyledTitle>
+          <StyledDescription>{description}</StyledDescription>
+        </StyledTextContainer>
+        <StyledImageContainer
+          width={videoImage.width}
+          height={videoImage.height}
+        >
+          <Image
+            src={videoImage.src}
+            alt={videoImage.alt}
+            layout="fill"
+            objectFit="contain"
+          />
+        </StyledImageContainer>
+        <StyledCardsContainer>
+          {cards.map((card) => (
+            <StyledCard key={card.title} {...card} />
+          ))}
+        </StyledCardsContainer>
+      </StyledContainer>
+    </>
+  );
 };
