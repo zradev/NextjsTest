8a0daadac5e1dce3c7cbdd5323fddd4606ccf4a2
zradev <zlatkoradevv@gmail.com>
2022-10-01 16:57:37 +0300

    Add Agency styled components

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,52 @@ const heroProps = {
   ctaText: "Start",
 };
 const agencyProps = {
-  image: { src: "/img/heroimg.png", alt: "", width: 1000, height: 1680 },
-  title: "Agency procurement, outsourced.",
-  description: "Start the process here",
-  ctaText: "Start",
+  title: "Managed agency selection",
+  description: "Strengthen your onboarding process",
+  videoImage: {
+    src: "/img/video.png",
+    alt: "background",
+    width: 300,
+    height: 520,
+  },
+  cards: [
+    {
+      title: "Brief",
+      description: (
+        <>
+          Complete <strong>brief writing or simple guidance</strong> on what to
+          include, we`ve got you covered
+        </>
+      ),
+      image: { src: "/img/brief.png", alt: "card", width: 45, height: 45 },
+      background: "#F9F9F9",
+      width: "90%",
+    },
+    {
+      title: "Search",
+      description: (
+        <>
+          In-depth agency search covering; <strong>criteria matching</strong>,
+          door knocking and due-dilligence vetting.
+        </>
+      ),
+      image: { src: "/img/search.png", alt: "card", width: 45, height: 45 },
+      background: "#F4F4F4",
+      width: "95%",
+    },
+    {
+      title: "Pitch",
+      description: (
+        <>
+          Comprehensive <strong>pitch management</strong>, including comms,
+          diary management and pitch hosting.
+        </>
+      ),
+      image: { src: "/img/pitch.png", alt: "card", width: 45, height: 45 },
+      background: "#F9F9F9",
+      width: "100%",
+    },
+  ],
 };
 
 export default function Home() {
--- sections/Agency/elements.js
+++ sections/Agency/elements.js
@@ -1,3 +1,81 @@
-import React from "react";
+import styled from "styled-components";
+import { Card } from "../../collections/Card/Card";
+import {
+  SectionContainer,
+  SectionHeading,
+  SectionSubheading,
+} from "~/components";
 
-export const StyledAgency = () => <div>Agency</div>;
+export const StyledContainer = styled((props) => (
+  <SectionContainer {...props} />
+))`
+  max-width: 100%;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 35px;
+  background-image: url("/img/background.png");
+  @media (max-width: 1024px) {
+    flex-direction: column;
+  }
+`;
+
+export const StyledTextContainer = styled((props) => <div {...props} />)`
+  width: 100%;
+  justify-content: center;
+  text-align: center;
+  margin-top: 50px;
+  h2 {
+    line-height: 3.5rem;
+  }
+  h3 {
+    font-weight: 400;
+  }
+`;
+
+export const StyledImageContainer = styled(({ height, width, ...props }) => (
+  <div {...props} />
+))`
+  position: relative;
+  width: ${({ width }) => width}px;
+  height: ${({ height }) => height}px;
+  margin: 0 20px 0 20px;
+`;
+
+export const StyledCardsContainer = styled((props) => <div {...props} />)`
+  display: flex;
+  flex-direction: column;
+  max-width: 472px;
+  justify-content: center;
+  align-items: flex-start;
+  @media (max-width: 1024px) {
+    max-width: 100%;
+  }
+`;
+
+export const StyledCard = styled(({ width, background, ...props }) => (
+  <Card {...props} />
+))`
+  width: ${(width) => width};
+  background-color: ${(background) => background};
+  border: 2px solid transparent;
+  &:hover {
+    background-color: #f1f1f1;
+    border: 2px solid #1a71f8;
+    border-radius: 8px;
+    h2 {
+      color: #1a71f8;
+    }
+  }
+`;
+
+export const StyledTitle = styled((props) => <SectionHeading {...props} />)`
+  margin: 0;
+  color: black;
+`;
+export const StyledDescription = styled((props) => (
+  <SectionSubheading {...props} />
+))`
+  margin: 0;
+  color: black;
+`;
